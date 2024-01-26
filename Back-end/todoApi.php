25437aa09cf269c6d9ccfdbe413d1632eb172639
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-26 16:11:15 +0100

    Eliminazione vecchio metodo di innestare manualmente array

--- Back-end/todoApi.php
+++ Back-end/todoApi.php
@@ -4,33 +4,6 @@
     header("Access-Control-Allow-Headers: X-Requested-With");
     header("Access-Control-Allow-Origin: http://localhost:5173");
 
-    $todos = [
-        [
-            'task' => 'Buy Groceries',
-            'done' =>  true,
-        ],
-        [
-            'task' => 'Walk the dog',
-            'done' => false,
-        ],
-        [
-            'task' => 'Wash the car',
-            'done' =>  true,
-        ],
-        [
-            'task' => 'Work on new application project',
-            'done' =>  true,
-        ],
-        [
-            'task' => 'Read new vue book',
-            'done' => false,
-        ],
-        [
-            'task' => 'Find answer to 42 question',
-            'done' =>  true,
-        ],
-    ];
-
-    $jsonTodos = json_encode($todos);
+    $jsonTodos = file_get_contents('todos.json');
     echo $jsonTodos;
 ?>
